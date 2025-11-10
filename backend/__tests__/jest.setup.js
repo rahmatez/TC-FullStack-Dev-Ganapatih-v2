0d82ab21f5b50b8a2ba025b50995867eab1c65d8
Rahmat Ashari <rahmatezdev@gmail.com>
2025-11-10 10:03:35 +0700

    Add Prisma disconnect in jest global teardown

--- backend/__tests__/jest.setup.js
+++ backend/__tests__/jest.setup.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
 // Load test environment variables
 dotenv.config();
@@ -10,3 +11,9 @@ if (process.env.DATABASE_URL_TEST) {
 
 // Set NODE_ENV to test
 process.env.NODE_ENV = 'test';
+
+// Global teardown - disconnect all Prisma instances after all tests
+afterAll(async () => {
+  const prisma = new PrismaClient();
+  await prisma.$disconnect();
+});
